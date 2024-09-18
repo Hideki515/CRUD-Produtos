e3c4342fbf8f7ab0f122c240e8d26ad13d842a51
Bruno Hideki Sato Misufara <brunohideki515@gmail.com>
2024-09-18 19:51:07 -0300

    🆙 Upade: Excluir Usuário

--- excluirUsuario.php
+++ excluirUsuario.php
@@ -1,10 +1,13 @@
 <?php
+// Inicia a sessão
 session_start();
+// Inclui sanatizar.php no código
 include_once 'sanitizar.php';
 
 $dados = sanitizar($_GET); //Sanitização 
-$loginUsuario = $dados['id'];
+$loginUsuario = $dados['id']; // Pega o id do Login
 
-$_SESSION['form'] = $dados;
+$_SESSION['form'] = $dados; 
 
+// Chama o excluirUsuarioForm.php
 header("Location:excluirUsuarioForm.php");
