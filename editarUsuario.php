41a840a7339027772d5270205aa4e17b93911b56
Bruno Hideki Sato Misufara <brunohideki515@gmail.com>
2024-09-18 20:02:56 -0300

    🆙 Update: Editar Usuário

--- editarUsuario.php
+++ editarUsuario.php
@@ -1,14 +1,13 @@
 <?php
+// Inicia a sessao
 session_start();
+// Inclui no código sanatizzer.php
 include_once 'sanitizar.php';
 
 $dados = sanitizar($_GET); //Sanitização 
-$loginUsuario = $dados['id'];
+$loginUsuario = $dados['id'];  // Pega o id de Login
 
-echo $loginUsuario;
-
-//Se chegou aqui é porque validou
-//Então busca os dados do Produto pelo id
+//Então busca os dados do Usuário pelo id
 //Credenciais para conexão com o Banco
 $dbhost = 'localhost:3306';
 $dbuser = 'root';
@@ -18,25 +17,29 @@ $dbname = 'crudproduto';
 //Abre conexão com o MySQL
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
+// Verifica se ocorreu erro na conexão com o banco
 if ($conn->connect_error) {
   die('Falha ao conectar com o MySQL: ' . $conn->connect_error);
 }
 
+// Faz a seleção dos dados do usuário a ser editado com base no login
 $sql = "SELECT * FROM Usuario WHERE login = '{$loginUsuario}'";
 $result = mysqli_query($conn, $sql); //A query seleciona as linhas da Tabela
 
+// Retorna erro ao recuperar dados do usuário
 if (mysqli_affected_rows($conn) != 1) {
   die('Falha ao recuperar dados do Usuario');
 }
 
-$produto = mysqli_fetch_assoc($result);
+// Obtém a próxima linha de resultado da consulta como um array associativo
+$usuario = mysqli_fetch_assoc($result);
 
-//echo '<pre>';
-//print_r($produto);
-//echo '</pre>';
+// Armazena os dados do usuário na sessão
+// Isso permite que os dados fiquem disponíveis em outras páginas
+$_SESSION['form'] = $usuario;
 
-$_SESSION['form'] = $produto;
-header("Location:editarUsuarioForm.php");
-
-
+// Fecha a conexão com o Banco
 $conn->close(); //Fecha a conexão com o Banco
+
+// Chama a página de Formulário de Edição
+header("Location:editarUsuarioForm.php");
