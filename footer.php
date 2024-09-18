ad60ed0f92bc02460f504c8a9bdf273bd13765e8
Bruno Hideki Sato Misufara <brunohideki515@gmail.com>
2024-09-18 20:24:28 -0300

    🆙 Update: Footer

--- footer.php
+++ footer.php
@@ -1,15 +1,16 @@
-        <footer class="footer mt-auto py-3 bg-body-tertiary">
+        <!-- <footer class="footer mt-auto py-3 bg-body-tertiary">
           <div class="container">
             <span class="text-body-secondary">#Rodapé</span>
           </div>
+        </footer> -->
+
+        <footer class="footer mt-auto py-3 bg-body-tertiary">
+          <div class="container text-center">
+            <span class="text-body-secondary">&copy; 2024 - Todos os direitos reservados - Bruno Hideki Sato Misufara | Diogo Ferreira Martins</span>
+          </div>
         </footer>
 
-        <!-- <script src="http://localhost/CRUD-Produto/js/bootstrap.bundle.min.js"></script> -->
         <script src="http://localhost/CRUD-Produtos/js/bootstrap.bundle.min.js"></script>
-        <!-- <script src="http://localhost/CRUD-Produtos/js/jquery-3.7.1.min.js"></script> -->
-        <!-- <script src="http://localhost/CRUD-Produtos/validarUsuario.js"></script> -->
-        <!-- <script src="js/jquery-3.7.1.min.js"></script> -->
-        <!-- <script src="validarUsuario.js"></script> -->
         </body>
 
         </html>
